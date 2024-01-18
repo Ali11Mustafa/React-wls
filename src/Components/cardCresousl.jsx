92042ac5101e0af059ed4eeaf588a7f5aed19082
Ali11Mustafa <ali.00622274@gmail.com>
2024-01-18 15:29:31 +0200

    changes

--- src/Components/cardCresousl.jsx
+++ src/Components/cardCresousl.jsx
@@ -72,7 +72,7 @@ function LatestProducts() {
   };
 
   return (
-    <div className="w-full md:scale-[75%]  sm:-translate-x-10 md:translate-x-0  lg:-translate-x-10">
+    <div className="w-full md:scale-[70%]  sm:-translate-x-10 md:translate-x-0  lg:-translate-x-10">
       <Slider {...settings}>
         {data.map((data, idx) => (
           <div key={idx}>
